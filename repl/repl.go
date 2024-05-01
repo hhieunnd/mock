f4477133cbd7006a6fa9f48223181ec1f744bd81
hieubk <hieundpython@gmail.com>
2024-05-01 17:01:08 +0700

    add parser funtion, call, if-else statement

--- repl/repl.go
+++ repl/repl.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"io"
 	"mock/lexer"
-	"mock/token"
+	"mock/parser"
 )
 
 const PROMPT = ">> "
@@ -14,17 +14,26 @@ func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
 
 	for {
-		fmt.Printf(PROMPT)
+		fmt.Print(PROMPT)
 		scanned := scanner.Scan()
 		if !scanned {
 			return
 		}
-
 		line := scanner.Text()
 		l := lexer.New(line)
-
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		p := parser.New(l)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
